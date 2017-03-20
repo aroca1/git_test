614347952f98830f0221e3b0ae3510469aac26af
Antonio Roca <antonio.roca@asu.edu>
2017-03-20 12:40:12 -0500

    finished test_branch after merging in hotfix

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ console.log('Welcome to version 1.1! This is for the branching part of the tutor
 console.log('at this point I have just created the hotfix branch, worked in it, merged it to master, and pushed master to origin. now i have returned to my test_branch work');
 console.log('this is my hotfix');
 console.log('now i am merging master into test_branch so that I can work with my updated material (with a hotfix)');
+console.log('alright, now i am done working on test_branch, so i will commit, push, and merge into master');
