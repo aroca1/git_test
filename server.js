e533db8ce2722f761f0324b65d090b8b3bd3fb9e
Antonio Roca <antonio.roca@asu.edu>
2017-03-20 12:32:49 -0500

    back to work after hotfix push

--- server.js
+++ server.js
@@ -1,2 +1,3 @@
 console.log('hey thre werld');
 console.log('Welcome to version 1.1! This is for the branching part of the tutorial');
+console.log('at this point I have just created the hotfix branch, worked in it, merged it to master, and pushed master to origin. now i have returned to my test_branch work');
